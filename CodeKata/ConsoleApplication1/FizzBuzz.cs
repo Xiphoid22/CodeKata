9fd911b404fd63131d01106bbf52ed6e877250bb
Lee <lee.s.marco@gmail.com>
2014-08-16 00:26:16 +0100

    Oops...FizzBuzz + Unit Tests

---
+++ CodeKata/ConsoleApplication1/FizzBuzz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class FizzBuzzKata 
+{
+    public string Answer(int i)
+    {
+        string answer = "";
+        if (i % 3 == 0) { answer += "Fizz"; }
+        if (i % 5 == 0) { answer += "Buzz"; }
+        return answer;
+    }
+}
---
+++ CodeKata/ConsoleApplication1/Tests/FizzBuzzTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeKata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        FizzBuzzKata fizzBuzz;
+
+        [TestFixtureSetUp]
+        public void init()
+        {
+            fizzBuzz = new FizzBuzzKata();
+        }
+
+        [Test]
+        public void fizzTest()
+        {
+            Assert.AreEqual("Fizz", fizzBuzz.Answer(3));
+        }
+
+        [Test]
+        public void BuzzTest()
+        {
+            Assert.AreEqual("Buzz", fizzBuzz.Answer(5));
+        }
+
+        [Test]
+        public void fizzBuzzTest()
+        {
+            Assert.AreEqual("FizzBuzz", fizzBuzz.Answer(15));
+        }
+
+        [Test]
+        public void NotDivisibleTest()
+        {
+            Assert.AreEqual("", fizzBuzz.Answer(7));
+        }
+
+    }
+}
