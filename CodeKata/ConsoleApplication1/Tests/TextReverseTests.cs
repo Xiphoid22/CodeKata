d483124574243ce7fa7b30b343dd675461c8c00b
Lee <lee.s.marco@gmail.com>
2014-08-16 09:20:18 +0100

    Text reverse kata + tests

---
+++ CodeKata/ConsoleApplication1/Tests/TextReverseTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeKata.Tests
+{
+    [TestFixture]
+    public class TextReverseTests
+    {
+        TextReverse reverse;
+
+        [TestFixtureSetUp]
+        public void init()
+        {
+            reverse = new TextReverse();
+        }
+
+        [Test]
+        public void HelloTest()
+        {
+            Assert.AreEqual("hello", reverse.Reverse("olleh"));
+        }
+
+        [Test]
+        public void OneCharacterTest()
+        {
+            Assert.AreEqual("a", reverse.Reverse("a"));
+        }
+
+        [Test]
+        public void EmptyStringTest()
+        {
+            Assert.AreEqual("", reverse.Reverse(""));
+        }
+    }
+}
---
+++ CodeKata/ConsoleApplication1/TextReverse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class TextReverse
+    {
+        public string Reverse(string input)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length ; i++)
+            {
+                output += input[input.Length - i - 1];
+            }
+            return output;
+        }
+    }
