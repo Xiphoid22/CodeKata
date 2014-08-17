0f41ef2d5729ae90e9a69ee9b3beaa544d746a69
Lee <lee.s.marco@gmail.com>
2014-08-17 09:55:24 +0100

    Now adding the files...

---
+++ CodeKata/ConsoleApplication1/TeaParty.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    class TeaParty
+    {
+        public string Welcome(string lastName, bool isWoman, bool isKnighted)
+        {
+            string greeting = "";
+            string title = "";
+            //Greet person with Hello X Y where X is Mr, Ms or Sir/Dame and Y is surname
+            if (isWoman == false)
+            {
+                if (isKnighted == false)
+                {
+                    title = "Mr.";
+                }
+                else
+                {
+                    title = "Sir";
+                }
+            }
+            else
+            {
+                if (isKnighted == false)
+                {
+                    title = "Ms.";
+
+                }
+                else
+                {
+                    title = "Lady";
+                }
+            }
+            greeting = String.Format("Hello {0} {1}", title, lastName);
+            return greeting;
+        }
+    }
+}
---
+++ CodeKata/ConsoleApplication1/Tests/TeaPartyTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeKata.Tests
+{
+    [TestFixture]
+    public class TeaPartyTests
+    {
+        TeaParty teaParty;
+
+        [TestFixtureSetUp]
+        public void init()
+        {
+            teaParty = new TeaParty();
+        }
+
+        [Test]
+        public void mrTest()
+        {
+            Assert.AreEqual("Hello Mr. Marco", teaParty.Welcome("Marco",false,false));
+        }
+
+        [Test]
+        public void msTest()
+        {
+            Assert.AreEqual("Hello Ms. Hufflepuff", teaParty.Welcome("Hufflepuff", true, false));
+        }
+
+        [Test]
+        public void sirTest()
+        {
+            Assert.AreEqual("Hello Sir Auron", teaParty.Welcome("Auron", false, true));
+        }
+
+        [Test]
+        public void LadyTest()
+        {
+            Assert.AreEqual("Hello Lady Akatsuki", teaParty.Welcome("Akatsuki", true, true));
+        }
+
+    }
+}
