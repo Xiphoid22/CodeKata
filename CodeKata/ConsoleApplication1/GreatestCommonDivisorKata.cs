9a24b91a1535f64e86753f347272c6f039027b07
Lee <lee.s.marco@gmail.com>
2014-08-19 21:44:52 +0100

    Added GCD Kata and Tests

---
+++ CodeKata/ConsoleApplication1/GreatestCommonDivisorKata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    public class GreatestCommonDivisorKata : IGreatestCommonDivisorKata
+    {
+        public int d = 1;
+
+        public int CalcGcd(int number1, int number2)
+        {
+            return number2 == 0 ? number1 : CalcGcd(number2, number1 % number2);
+        }
+    }
+
+    public interface IGreatestCommonDivisorKata
+    {
+        int CalcGcd(int number1, int number2);
+    }
+}
---
+++ CodeKata/ConsoleApplication1/Tests/GCDTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeKata.Tests
+{
+    [TestFixture]
+    public class GCDTests
+    {
+
+        GreatestCommonDivisorKata GCD;
+
+        [TestFixtureSetUp]
+        public void init()
+        {
+            GCD = new GreatestCommonDivisorKata();
+        }
+
+        [Test]
+        public void SecondNumberLargerTest()
+        {
+            Assert.AreEqual(6, GCD.CalcGcd(12,18));
+        }
+
+        [Test]
+        public void FirstNumberLargerTest()
+        {
+            Assert.AreEqual(5, GCD.CalcGcd(55, 10));
+        }
+
+        [Test]
+        public void ZeroFirstNumberTest()
+        {
+            Assert.AreEqual(10, GCD.CalcGcd(0, 10));
+        }
+
+        [Test]
+        public void ZeroSecondNumberTest()
+        {
+            Assert.AreEqual(55, GCD.CalcGcd(55, 0));
+        }
+    }
+}
