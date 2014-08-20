af59e77face2b2a95e80ff675d0692c11243f29b
Lee <lee.s.marco@gmail.com>
2014-08-20 20:21:01 +0100

    Started PlayingCards Kata. Added the ability to create a deck of cards. Also added tests!

---
+++ CodeKata/ConsoleApplication1/PlayingCards.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    class PlayingCards
+    {
+        public List<Card> CreateDeck()
+        {
+            var suits = new string[4] {"Spades","Hearts","Clubs","Diamonds"};
+            var value = new string[13] {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+            var deck = new List<Card>();
+
+            for (var i = 0; i < suits.Length; i++)
+            {
+                for (var j = 0; j < value.Length; j++)
+                {
+                    var card = new Card(suits[i], value[j]);
+                    deck.Add(card);
+                }
+            }
+            return deck;
+        }
+
+        public void ShuffleDeck(string[] deck)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Card> CreateHand(List<Card> deck)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Card
+    {
+        private readonly string _suit;
+        private readonly string _value;
+
+        public Card(string suit, string value)
+        {
+            _suit = suit;
+            _value = value;
+        }
+
+        public string GetSuit()
+        {
+            return _suit;
+        }
+
+        public string GetValue()
+        {
+            return _value;
+        }
+    }
+}
---
+++ CodeKata/ConsoleApplication1/Tests/PlayingCardTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CodeKata.Tests
+{
+    [TestFixture]
+    internal class PlayingCardTests
+    {
+        private PlayingCards cards;
+
+        [TestFixtureSetUp]
+        public void init()
+        {
+            cards = new PlayingCards();
+        }
+
+        [Test]
+        public void CreateDeckTest()
+        {
+            var deck = cards.CreateDeck();
+            Assert.IsTrue(deck.Count == 52);
+        }
+
+        [Test]
+        public void CheckSuit()
+        {
+            var deck = cards.CreateDeck();
+            Assert.IsTrue(deck[0].GetSuit() == "Spades");
+        }
+
+        [Test]
+        public void CheckValue()
+        {
+            var deck = cards.CreateDeck();
+            Assert.IsTrue(deck[0].GetValue() == "Ace");
+        }
+    }
+}
