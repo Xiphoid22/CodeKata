6499386ebd95f34b73400003efa35182ccf0d260
Lee <lee.s.marco@gmail.com>
2014-08-25 10:13:16 +0100

    Added start of String Calculator Kata and tests http://osherove.com/tdd-kata-1/

---
+++ CodeKata/ConsoleApplication1/StringCalculatorKata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeKata
+{
+    class StringCalculatorKata
+    {
+        public int Add(string numbers)
+        {
+            var splitNumbers = numbers.Split(',');
+
+            try
+            {
+                return splitNumbers.Sum(number => Convert.ToInt32(number));
+            }
+            catch (FormatException)
+            {
+                return 0;
+            }
+            
+        }
+    }
+}
---
+++ CodeKata/ConsoleApplication1/Tests/StringCalculatorTests.cs
@@ -0,0 +1,52 @@
+﻿
+using NUnit.Framework;
+
+namespace CodeKata.Tests
+{
+    [TestFixture]
+    class StringCalculatorTests
+    {
+        StringCalculatorKata _stringCalculatorKata;
+
+        [TestFixtureSetUp]
+        public void init()
+        {
+            _stringCalculatorKata = new StringCalculatorKata();
+        }
+
+        [Test]
+        public void AddZeroNumber()
+        {
+            var result = _stringCalculatorKata.Add("");
+            Assert.AreEqual(result, 0);
+        }
+
+        [Test]
+        public void AddOneNumber()
+        {
+            var result = _stringCalculatorKata.Add("1");
+            Assert.AreEqual(result, 1);
+        }
+
+        [Test]
+        public void AddTwoNumbers()
+        {
+            var result = _stringCalculatorKata.Add("1,2");
+            Assert.AreEqual(result, 3);
+        }
+
+        [Test]
+        public void AddNegativeNumbers()
+        {
+            var result = _stringCalculatorKata.Add("-1,-2");
+            Assert.AreEqual(result, -3);
+        }
+
+        [Test]
+        public void AddMoreThanTwoNumbers()
+        {
+            var result = _stringCalculatorKata.Add("1,5,17,-10,11,-4");
+            Assert.AreEqual(result, 20);
+        }
+    }
+}
