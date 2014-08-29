4d66934bc4f199a80ba3f54b44697db950210a24
Lee <lee.s.marco@gmail.com>
2014-08-29 20:59:16 +0100

    More string calculator Kata! This time some refactoring to split out the massive add function. Also did the next part of the kata meaning we no longer accept negative numbers.

--- CodeKata/ConsoleApplication1/StringCalculatorKata.cs
+++ CodeKata/ConsoleApplication1/StringCalculatorKata.cs
@@ -6,31 +6,18 @@ namespace CodeKata
 {
     class StringCalculatorKata
     {
-        public int Add(string numbers)
+        private string _numbers;
+
+        public StringCalculatorKata(string numbers)
         {
-            string[] delimiters;
+            _numbers = numbers;
+        }
 
-            const string pattern = @"(?<=//).";
-            var regex = new Regex(pattern);
-
-            if (regex.Matches(numbers).Count > 0)
-            { 
-                var customDelimiter = regex.Match(numbers).ToString();
-                delimiters = new string[] { ",", "\n", customDelimiter};
-
-                //Remove the custom delimiter section from the string
-                var positionOfFirstLineBreak = numbers.IndexOf("\n", StringComparison.Ordinal);
-                if (positionOfFirstLineBreak > 0)
-                {
-                    numbers = numbers.Remove(0, positionOfFirstLineBreak + 1);
-                }
-            }
-            else
-            {
-                delimiters = new string[] { ",", "\n"}; 
-            }
-
-            var splitNumbers = numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        public int Add()
+        {
+            var delimiters = SetDelimiters();
+            var splitNumbers = _numbers.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            CheckForNegativeNumbers(splitNumbers);
 
             try
             {
@@ -40,7 +27,51 @@ namespace CodeKata
             {
                 return 0;
             }
-            
+        }
+
+        internal string[] SetDelimiters()
+        {
+            const string pattern = @"(?<=//).";
+            var regex = new Regex(pattern);
+
+            if (regex.Matches(_numbers).Count > 0)
+            {
+                var customDelimiter = regex.Match(_numbers).ToString();
+                RemoveDelimiterSection();
+                return new[] { ",", "\n", customDelimiter };
+            }
+
+            return new[] { ",", "\n" };
+        }
+
+        internal void RemoveDelimiterSection()
+        {
+            var positionOfFirstLineBreak = _numbers.IndexOf("\n", StringComparison.Ordinal);
+            if (positionOfFirstLineBreak > 0)
+            {
+                _numbers = _numbers.Remove(0, positionOfFirstLineBreak + 1);
+            }
+        }
+
+        internal void CheckForNegativeNumbers(string[] splitNumbers)
+        {
+            var negativeNumbers = "";
+            foreach (var number in splitNumbers.Where(x => Convert.ToInt32(x) < 0))
+            {
+                negativeNumbers = negativeNumbers + number + " ";
+
+            }
+            if (negativeNumbers != "")
+            {
+                throw new NegativeNumbersException("Negatives not allowed. " + negativeNumbers.Trim());
+            }
+        }
+    }
+
+    public class NegativeNumbersException : Exception
+    {
+        public NegativeNumbersException(string message) : base(message)
+        {
         }
     }
 }
--- CodeKata/ConsoleApplication1/Tests/StringCalculatorTests.cs
+++ CodeKata/ConsoleApplication1/Tests/StringCalculatorTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using NUnit.Framework;
 
 namespace CodeKata.Tests
@@ -8,79 +9,83 @@ namespace CodeKata.Tests
     {
         StringCalculatorKata _stringCalculatorKata;
 
-        [TestFixtureSetUp]
-        public void init()
-        {
-            _stringCalculatorKata = new StringCalculatorKata();
-        }
-
         [Test]
         public void AddZeroNumber()
         {
-            var result = _stringCalculatorKata.Add("");
+            _stringCalculatorKata = new StringCalculatorKata("");
+            var result = _stringCalculatorKata.Add();
             Assert.AreEqual(result, 0);
         }
 
         [Test]
         public void AddOneNumber()
         {
-            var result = _stringCalculatorKata.Add("1");
+            _stringCalculatorKata = new StringCalculatorKata("1");
+            var result = _stringCalculatorKata.Add();
             Assert.AreEqual(result, 1);
         }
 
         [Test]
         public void AddTwoNumbers()
         {
-            var result = _stringCalculatorKata.Add("1,2");
+            _stringCalculatorKata = new StringCalculatorKata("1,2");
+            var result = _stringCalculatorKata.Add();
             Assert.AreEqual(result, 3);
         }
 
         [Test]
         public void AddNegativeNumbers()
         {
-            var result = _stringCalculatorKata.Add("-1,-2");
-            Assert.AreEqual(result, -3);
+            _stringCalculatorKata = new StringCalculatorKata("-1,-2");
+            var exception = Assert.Throws<NegativeNumbersException>(() => _stringCalculatorKata.Add());
+            Assert.That(exception.Message, Is.EqualTo("Negatives not allowed. -1 -2"));
         }
 
         [Test]
         public void AddMoreThanTwoNumbers()
         {
-            var result = _stringCalculatorKata.Add("1,5,17,-10,11,-4");
-            Assert.AreEqual(result, 20);
+            _stringCalculatorKata = new StringCalculatorKata("1,5,17,10,11,4");
+            var result = _stringCalculatorKata.Add();
+            Assert.AreEqual(result, 48);
         }
 
         [Test]
         public void CommasOrNewLineDelimiter()
         {
-            var result = _stringCalculatorKata.Add("1\n5,17,-10\n11\n-4");
-            Assert.AreEqual(result, 20);
+            _stringCalculatorKata = new StringCalculatorKata("1\n5,17,10\n11\n4");
+            var result = _stringCalculatorKata.Add();
+            Assert.AreEqual(result, 48);
         }
 
         [Test]
         public void NewLineDelimiter()
         {
-            var result = _stringCalculatorKata.Add("\n1\n5\n17\n-10\n11\n-4");
-            Assert.AreEqual(result, 20);
+            _stringCalculatorKata = new StringCalculatorKata("\n1\n5\n17\n10\n11\n4");
+            var result = _stringCalculatorKata.Add();
+            Assert.AreEqual(result, 48);
         }
 
         [Test]
         public void CustomDelimiter()
         {
-            var result = _stringCalculatorKata.Add("//;\n1;2");
+            _stringCalculatorKata = new StringCalculatorKata("//;\n1;2");
+            var result = _stringCalculatorKata.Add();
             Assert.AreEqual(result, 3);
         }
 
         [Test]
         public void CustomnDelimiter()
         {
-            var result = _stringCalculatorKata.Add("//n\n1n2n3");
+            _stringCalculatorKata = new StringCalculatorKata("//n\n1n2n3");
+            var result = _stringCalculatorKata.Add();
             Assert.AreEqual(result, 6);
         }
 
         [Test]
         public void CustomSlashDelimiter()
         {
-            var result = _stringCalculatorKata.Add("//\\\n1\\2\\3");
+            _stringCalculatorKata = new StringCalculatorKata("//\\\n1\\2\\3");
+            var result = _stringCalculatorKata.Add();
             Assert.AreEqual(result, 6);
         }
     }
